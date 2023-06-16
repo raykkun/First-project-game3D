59326d6db56cd4d132b056fd0879df4aa4c38c35
raykkun <raiwijaya2016@gmail.com>
2023-06-16 14:45:34 +0700

    First commit

---
+++ Assets/script/Rotate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] float speedX;
+    [SerializeField] float speedY;
+    [SerializeField] float speedZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(360 * speedX * Time.deltaTime, 360 * speedY * Time.deltaTime, 360 * speedZ * Time.deltaTime);
+    }
+}
